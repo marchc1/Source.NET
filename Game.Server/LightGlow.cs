65e0de7fa1ef130ea1bb92573d2000bc44c647b0
march <106459595+marchc1@users.noreply.github.com>
2025-10-12 16:09:47 -0700

    Add LightGlow

---
+++ Game.Server/LightGlow.cs
@@ -0,0 +1,39 @@
+﻿using Source.Common;
+using Source;
+
+using Game.Shared;
+using System.Numerics;
+using Source.Common.MaterialSystem;
+
+namespace Game.Server;
+
+
+using FIELD = FIELD<LightGlow>;
+
+public class LightGlow : BaseEntity
+{
+	public static readonly SendTable DT_LightGlow = new([
+		SendPropInt(FIELD.OF(nameof(RenderColor)), 32, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(HorizontalSize)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(VerticalSize)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(MinDist)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(MaxDist)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(OuterMaxDist)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(SpawnFlags)), 8, PropFlags.Unsigned),
+		SendPropVector(FIELD.OF(nameof(Origin)), 0, PropFlags.Coord),
+		SendPropVector(FIELD.OF(nameof(Rotation)), 13, PropFlags.RoundDown, 0, 360),
+		SendPropEHandle(FIELD.OF(nameof(MoveParent))),
+		SendPropFloat(FIELD.OF(nameof(GlowProxySize)), 6, PropFlags.RoundUp, 1, 64),
+		SendPropFloat(FIELD.OF(nameof(HDRColorScale)), 0, PropFlags.NoScale),
+	]);
+	public static readonly new ServerClass ServerClass = new ServerClass("LightGlow", DT_LightGlow).WithManualClassID(StaticClassIndices.CLightGlow);
+
+	public Color RenderColor;
+	public int HorizontalSize;
+	public int VerticalSize;
+	public int MinDist;
+	public int MaxDist;
+	public int OuterMaxDist;
+	public float GlowProxySize;
+	public float HDRColorScale;
+}
