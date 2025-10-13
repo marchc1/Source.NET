d6f1988fd18218a94da5d73a7bddba8964d4ebc1
callumok2004 <callumokane123@gmail.com>
2025-10-13 18:32:10 +0100

    Add PropertyPage

---
+++ Source.GUI.Controls/PropertyPage.cs
@@ -0,0 +1,43 @@
+﻿using Source.Common.Input;
+
+namespace Source.GUI.Controls;
+
+public class PropertyPage : EditablePanel
+{
+	// Handle PageTab;
+	Panel? PageTab;
+
+	public PropertyPage(Panel? parent, string? name) : base(parent, name) {
+
+	}
+
+	public void OnResetData() { }
+	public void OnApplyChanges() { }
+	public void OnPageShow() { }
+	public void OnPageHide() { }
+
+	public void OnPageTabActivated(Panel pageTab) {
+		PageTab = pageTab;
+	}
+
+	public override void OnKeyCodeTyped(ButtonCode code) {
+		switch (code) {
+			case ButtonCode.KeyRight:
+			case ButtonCode.KeyLeft:
+				if (PageTab != null && PageTab.HasFocus())
+					base.OnKeyCodeTyped(code);
+				break;
+			default:
+				base.OnKeyCodeTyped(code);
+				break;
+		}
+	}
+
+	public override void SetVisible(bool state) {
+		if (IsVisible() && state)
+			if (GetFocusNavGroup().GetCurrentDefaultButton() != null)
+				GetFocusNavGroup().SetCurrentDefaultButton(null);
+
+		base.SetVisible(state);
+	}
+}
