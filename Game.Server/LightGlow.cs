7e13366157568c410a5db99b7e1abb61eadd51b6
march <106459595+marchc1@users.noreply.github.com>
2025-10-12 17:02:08 -0700

    Somehow messed up the file names

--- Game.Server/LightGlow.cs
+++ Game.Server/LightGlow.cs
@@ -2,37 +2,38 @@
 using Source;
 
 using Game.Shared;
+using System.Numerics;
+using Source.Common.MaterialSystem;
 
 namespace Game.Server;
 
 
-using FIELD = FIELD<FuncRotating>;
+using FIELD = FIELD<LightGlow>;
 
-public class FuncRotating : BaseEntity
+public class LightGlow : BaseEntity
 {
-	public static readonly SendTable DT_FuncRotating = new(DT_BaseEntity, [
-		SendPropExclude(nameof(DT_BaseEntity), nameof(Rotation)),
-		SendPropExclude(nameof(DT_BaseEntity), nameof(Origin)),
-		SendPropExclude(nameof(DT_BaseEntity), nameof(SimulationTime)),
-
-		SendPropVector(FIELD.OF(nameof(Origin)), 0, PropFlags.Coord | PropFlags.ChangesOften, 0, Constants.HIGH_DEFAULT, SendProxy_FuncRotatingOrigin),
-		SendPropAngle(FIELD.OF_VECTORELEM(nameof(Rotation), 0), 13, PropFlags.RoundDown | PropFlags.ChangesOften, proxyFn: SendProxy_FuncRotatingAngle),
-		SendPropAngle(FIELD.OF_VECTORELEM(nameof(Rotation), 1), 13, PropFlags.RoundDown | PropFlags.ChangesOften, proxyFn: SendProxy_FuncRotatingAngle),
-		SendPropAngle(FIELD.OF_VECTORELEM(nameof(Rotation), 2), 13, PropFlags.RoundDown | PropFlags.ChangesOften, proxyFn: SendProxy_FuncRotatingAngle),
-		SendPropInt(FIELD.OF(nameof(SimulationTime)), SIMULATION_TIME_WINDOW_BITS, PropFlags.Unsigned | PropFlags.ChangesOften | PropFlags.EncodedAgainstTickCount, SendProxy_FuncRotatingSimulationTime)
+	public static readonly SendTable DT_LightGlow = new([
+		SendPropInt(FIELD.OF(nameof(RenderColor)), 32, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(HorizontalSize)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(VerticalSize)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(MinDist)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(MaxDist)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(OuterMaxDist)), 16, PropFlags.Unsigned),
+		SendPropInt(FIELD.OF(nameof(SpawnFlags)), 8, PropFlags.Unsigned),
+		SendPropVector(FIELD.OF(nameof(Origin)), 0, PropFlags.Coord),
+		SendPropVector(FIELD.OF(nameof(Rotation)), 13, PropFlags.RoundDown, 0, 360),
+		SendPropEHandle(FIELD.OF(nameof(MoveParent))),
+		SendPropFloat(FIELD.OF(nameof(GlowProxySize)), 6, PropFlags.RoundUp, 1, 64),
+		SendPropFloat(FIELD.OF(nameof(HDRColorScale)), 0, PropFlags.NoScale),
 	]);
+	public static readonly new ServerClass ServerClass = new ServerClass("LightGlow", DT_LightGlow).WithManualClassID(StaticClassIndices.CLightGlow);
 
-	private static void SendProxy_FuncRotatingOrigin(SendProp prop, object instance, IFieldAccessor field, ref DVariant outData, int element, int objectID) {
-		throw new NotImplementedException();
-	}
-
-	private static void SendProxy_FuncRotatingAngle(SendProp prop, object instance, IFieldAccessor field, ref DVariant outData, int element, int objectID) {
-		throw new NotImplementedException();
-	}
-
-	private static void SendProxy_FuncRotatingSimulationTime(SendProp prop, object instance, IFieldAccessor field, ref DVariant outData, int element, int objectID) {
-		throw new NotImplementedException();
-	}
-
-	public static readonly new ServerClass ServerClass = new ServerClass("FuncRotating", DT_FuncRotating).WithManualClassID(StaticClassIndices.CFuncRotating);
+	public Color RenderColor;
+	public int HorizontalSize;
+	public int VerticalSize;
+	public int MinDist;
+	public int MaxDist;
+	public int OuterMaxDist;
+	public float GlowProxySize;
+	public float HDRColorScale;
 }
