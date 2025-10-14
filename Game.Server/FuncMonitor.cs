d0f22d31810fc526730af1f3be20808d368e7bb0
march <106459595+marchc1@users.noreply.github.com>
2025-10-13 22:13:13 -0700

    so did FuncMonitor

--- Game.Server/FuncMonitor.cs
+++ Game.Server/FuncMonitor.cs
@@ -4,7 +4,7 @@ using Source.Common;
 
 namespace Game.Server;
 
-public class FuncMonitor : Breakable
+public class FuncMonitor : FuncBrush
 {
 	public static readonly SendTable DT_FuncMonitor = new(DT_BaseEntity, []);
 	public static readonly new ServerClass ServerClass = new ServerClass("FuncMonitor", DT_FuncMonitor).WithManualClassID(StaticClassIndices.CFuncMonitor);
