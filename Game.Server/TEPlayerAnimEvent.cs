65675f6c14163df6314217a3e347b9defd27293f
march <106459595+marchc1@users.noreply.github.com>
2025-10-14 15:01:51 -0700

    Copy paste fail again

--- Game.Server/TEPlayerAnimEvent.cs
+++ Game.Server/TEPlayerAnimEvent.cs
@@ -12,7 +12,7 @@ public class TEPlayerAnimEvent : BaseTempEntity
 		SendPropInt(FIELD.OF(nameof(Event)), 6, PropFlags.Unsigned),
 		SendPropInt(FIELD.OF(nameof(Data)), 32)
 	]);
-	public static readonly new ServerClass ServerClass = new ServerClass("PhysMagnet", DT_TEPlayerAnimEvent).WithManualClassID(StaticClassIndices.CTEPlayerAnimEvent);
+	public static readonly new ServerClass ServerClass = new ServerClass("TEPlayerAnimEvent", DT_TEPlayerAnimEvent).WithManualClassID(StaticClassIndices.CTEPlayerAnimEvent);
 
 	public readonly EHANDLE Player = new();
 	public readonly int Event;
