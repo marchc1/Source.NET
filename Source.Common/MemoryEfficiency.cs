1b643f3d0bbbc5e8720d6abba4160d2a13e0cbdd
march <106459595+marchc1@users.noreply.github.com>
2025-11-16 11:55:16 -0800

    Create MemoryEfficiency.cs (experiment)

---
+++ Source.Common/MemoryEfficiency.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Source.Common;
+
+public enum StructInitializationMode {
+	None,
+	Default,
+	New
+}
+public class ReusableBox<T> where T : struct {
+	// Instance fields
+	public T Struct;
+
+	// Instance methods
+	public ref T Ref() => ref Struct;
+
+	// Static members
+	static readonly ClassMemoryPool<ReusableBox<T>> pool = new();
+	// Static methods
+	public static ReusableBox<T> Rent(StructInitializationMode initializeMode = StructInitializationMode.New) {
+		ReusableBox<T> box = pool.Alloc();
+
+		if (initializeMode == StructInitializationMode.New)
+			box.Struct = new();
+		else if (initializeMode == StructInitializationMode.Default)
+			box.Struct = default;
+
+		return box;
+	}
+
+	public static void Return(ReusableBox<T> box) {
+		pool.Free(box);
+	}
+}
