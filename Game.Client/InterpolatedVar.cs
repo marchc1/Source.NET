b1b7e60a1c67f857c63ba573a04aa0c19881aab8
march <106459595+marchc1@users.noreply.github.com>
2025-11-20 10:20:04 -0800

    Update InterpolatedVar.cs

--- Game.Client/InterpolatedVar.cs
+++ Game.Client/InterpolatedVar.cs
@@ -117,7 +117,7 @@ public class SimpleRingBuffer<T> where T : new()
 		count++;
 		int index = WrapRange(firstElement + count - 1);
 		elements![index] = new T();
-		return count - 1;
+		return index;
 	}
 
 	public void RemoveAll() {
