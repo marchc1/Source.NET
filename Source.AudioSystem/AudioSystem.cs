fef480bcba6e0d9b3a73d1372a1fda342c2e1461
march <106459595+marchc1@users.noreply.github.com>
2025-11-22 19:21:55 -0800

    More bass experiments

--- Source.AudioSystem/AudioSystem.cs
+++ Source.AudioSystem/AudioSystem.cs
@@ -1,10 +1,57 @@
 ﻿using ManagedBass;
 
 using Source.Common.Audio;
+using Source.Common.Filesystem;
 
 using System.Numerics;
 
+using static Source.AudioSystem.AudioGlobals;
+using static Source.AudioSystem.BassAudioFileSource;
 namespace Source.AudioSystem;
+
+[EngineComponent]
+public static class AudioGlobals
+{
+	[Dependency] public static IFileSystem filesystem = null!;
+}
+
+
+public class BassAudioSource : AudioSource
+{
+	public int BassStream = 0;
+}
+
+
+public class BassAudioFileSource : BassAudioSource, IDisposable
+{
+	public BassAudioFileSource(ReadOnlySpan<char> file) {
+		Span<char> fileSearch = stackalloc char[file.Length + 6];
+		"sound/".CopyTo(fileSearch);
+		file.CopyTo(fileSearch[6..]);
+
+		if (!filesystem.FileExists(fileSearch, "game"))
+			return;
+
+		IFileHandle? fh = filesystem.Open(fileSearch, FileOpenOptions.Read | FileOpenOptions.Binary, "game");
+		if (fh == null)
+			return;
+
+		byte[] data = new byte[fh.Stream.Length];
+		fh.Stream.ReadExactly(data);
+		fh.Dispose();
+
+		BassStream = Bass.CreateStream(data, 0, data.Length, BassFlags.Default);
+		if(BassStream == 0) {
+			Dbg.Msg($"BASS: {Bass.LastError}\n");
+		} 
+	}
+
+	public void Dispose() {
+		Bass.StreamFree(BassStream);
+		BassStream = 0;
+	}
+}
+
 
 public class AudioSystem : IAudioSystem
 {
@@ -44,7 +91,21 @@ public class AudioSystem : IAudioSystem
 	}
 
 	public long StartDynamicSound(in StartSoundParams parms) {
-		throw new NotImplementedException();
+		BassAudioSource src = PreloadSound(parms.Sfx);
+		Bass.ChannelPlay(src.BassStream);
+		return 0;
+	}
+
+	private BassAudioSource PreloadSound(SfxTable? sfx) {
+		if (sfx == null)
+			return null!;
+
+		if (sfx.Source != null)
+			return (BassAudioSource)sfx.Source;
+
+		var src = new BassAudioFileSource(sfx.GetFileName());
+		sfx.Source = src;
+		return src;
 	}
 
 	public long StartStaticSound(in StartSoundParams parms) {
