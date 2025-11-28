15485d055d5a844d64de24651377a8866ba2f359
march <106459595+marchc1@users.noreply.github.com>
2025-11-27 18:04:25 -0800

    CvarIgnore attribute

--- Source.Common/Commands/ICvar.cs
+++ Source.Common/Commands/ICvar.cs
@@ -5,6 +5,14 @@ namespace Source.Common.Commands;
 public delegate void FnChangeCallback(IConVar var, in ConVarChangeContext ctx);
 public delegate void FnChangeCallbackSrc(IConVar var, ReadOnlySpan<char> oldString, float oldFloat);
 
+// todo write a better summary later
+/// <summary>
+/// Marks a field/property as being a reference to a convar rather than a convar that the engine is supposed to register.
+/// By default, all class fields with the <see cref="ConVar"/> type are pulled into the engine. This attribute can suppress that.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class CvarIgnoreAttribute : Attribute;
+
 public interface IConsoleDisplayFunc
 {
 	public void ColorPrint(in Color clr, ReadOnlySpan<char> message);
