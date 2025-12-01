8db04893db19b806762c9a48e011b3b271fd8b1d
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-12-01 10:59:10 +0100

    Implement most of CLuaInterface
    [+] Mostly implemented CLuaInterface
    [+] Added lua_getuserdatatype
    [#] Updated to C++17

--- Lua.Shared/CSharpBridge.cpp
+++ Lua.Shared/CSharpBridge.cpp
@@ -1,8 +1,28 @@
 #include "csharpbridge.h"
+#include "CLuaInterface.h"
 
 CSharpInterface* g_pCSharpInterface;
 extern "C" void __cdecl SetupCSharpCallback(CSharpInterface* pInterface)
 {
 	// Called by C# before init
 	g_pCSharpInterface = pInterface;
+}
+
+// Exposed for C# to call for ILuaShared
+
+extern "C" void __cdecl LuaShared_Init()
+{
+	g_pCLuaShared->Init();
+}
+
+extern "C" void* __cdecl LuaShared_CreateLuaInterface(unsigned char nRealm)
+{
+	return g_pCLuaShared->CreateLuaInterface(nRealm, false);
+}
+
+// Exposed for C# to call for ILuaInterface
+
+extern "C" bool __cdecl LuaInterface_Init(void* pInterface, bool isServer)
+{
+	return ((ILuaInterface*)pInterface)->Init(g_pLuaGameCallback, isServer);
 }
