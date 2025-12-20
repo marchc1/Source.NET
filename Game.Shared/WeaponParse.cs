07a1d52759d5e6eee444cf7559eedc45166b2c1c
callumok2004 <callumokane123@gmail.com>
2025-12-20 02:25:32 +0000

    Move FileWeaponInfo to Shared

--- Game.Shared/WeaponParse.cs
+++ Game.Shared/WeaponParse.cs
@@ -1,6 +1,11 @@
-﻿namespace Game.Shared;
+﻿using Game.Client.HUD;
 
-public enum WeaponSound {
+using Source.Common.Formats.Keyvalues;
+
+namespace Game.Shared;
+
+public enum WeaponSound
+{
 	Empty,
 	Single,
 	SingleNPC,
@@ -17,4 +22,48 @@ public enum WeaponSound {
 	Special3,
 	Taunt,
 	Deploy
+}
+
+public class FileWeaponInfo
+{
+	bool ParsedScript;
+	bool LoadedHudElements;
+	string ClassName;
+	string PrintName;
+	string ViewModel;
+	string WorldModel;
+	string AnimationPrefix;
+	int Slot;
+	int Position;
+	int MaxClip1;
+	int MaxClip2;
+	int DefaultClip1;
+	int DefaultClip2;
+	int Weight;
+	int RumbleEffect;
+	bool AutoSWitchTo;
+	bool AutoSwitchFrom;
+	int Flags;
+	string Ammo1;
+	string Ammo2;
+	string[] ShootSounds = [];
+	int AmmoType;
+	int Ammo2Type;
+	bool bMeleeWeapon;
+	bool BuiltRightHanded;
+	bool AllowFlipping;
+	int SpriteCount;
+	HudTexture? IconActive;
+	HudTexture? IconInactive;
+	HudTexture? IconAmmo;
+	HudTexture? IconAmmo2;
+	HudTexture? IconCrosshair;
+	HudTexture? IconAutoaim;
+	HudTexture? IconZoomedCrosshair;
+	HudTexture? IconZoomedAutoaim;
+	HudTexture? IconSmall;
+
+	FileWeaponInfo() { }
+
+	void Parse(KeyValues keyValuesData, ReadOnlySpan<char> weaponName) { }
 }
