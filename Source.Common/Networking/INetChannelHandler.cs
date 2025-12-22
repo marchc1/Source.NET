ba346fb63c074adf94262e2722bf02dbd1230e65
march <106459595+marchc1@users.noreply.github.com>
2025-12-22 14:52:33 -0800

    Make these required interface methods

--- Source.Common/Networking/INetChannelHandler.cs
+++ Source.Common/Networking/INetChannelHandler.cs
@@ -2,15 +2,15 @@ namespace Source.Common.Networking;
 
 public interface INetChannelHandler
 {
-	void ConnectionStart(NetChannel channel) { }
-	void ConnectionClosing(ReadOnlySpan<char> reason) { }
-	void ConnectionCrashed(ReadOnlySpan<char> reason) { }
-	void PacketStart(int incomingSequence, int outgoingAcknowledged) { }
-	void PacketEnd() { }
-	void FileRequested(ReadOnlySpan<char> fileName, uint transferID) { }
-	void FileReceived(ReadOnlySpan<char> fileName, uint transferID) { }
-	void FileDenied(ReadOnlySpan<char> fileName, uint transferID) { }
-	void FileSent(ReadOnlySpan<char> fileName, uint transferID) { }
+	void ConnectionStart(NetChannel channel);
+	void ConnectionClosing(ReadOnlySpan<char> reason);
+	void ConnectionCrashed(ReadOnlySpan<char> reason);
+	void PacketStart(int incomingSequence, int outgoingAcknowledged);
+	void PacketEnd();
+	void FileRequested(ReadOnlySpan<char> fileName, uint transferID);
+	void FileReceived(ReadOnlySpan<char> fileName, uint transferID);
+	void FileDenied(ReadOnlySpan<char> fileName, uint transferID);
+	void FileSent(ReadOnlySpan<char> fileName, uint transferID);
 
-	bool ProcessMessage<T>(T message) where T : INetMessage => false;
+	bool ProcessMessage<T>(T message) where T : INetMessage;
 }
