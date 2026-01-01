2e709a7e4d8bb1f7cc0f2beee892137468df97fe
callumok2004 <callumokane123@gmail.com>
2026-01-01 16:52:35 +0000

    Some ScalableImagePanel impl

--- Source.GUI.Controls/ScalableImagePanel.cs
+++ Source.GUI.Controls/ScalableImagePanel.cs
@@ -1,4 +1,5 @@
 using Source.Common.Formats.Keyvalues;
+using Source.Common.GUI;
 
 namespace Source.GUI.Controls;
 
@@ -8,18 +9,57 @@ class ScalableImagePanel : Panel
 	int SrcCornerWidth;
 	int CornerHeight;
 	int CornerWidth;
-	int TextureID;
+	TextureID TextureID;
 	float CornerWidthPercent;
 	float CornerHeightPercent;
-	string ImageName;
-	string DrawColorName;
+	string? ImageName;
+	string? DrawColorName;
 	Color DrawColor;
 
 	public ScalableImagePanel(Panel? parent, ReadOnlySpan<char> panelName) : base(parent, panelName) {
+		SrcCornerHeight = 0;
+		SrcCornerWidth = 0;
 
+		CornerHeight = 0;
+		CornerWidth = 0;
+
+		ImageName = null;
+		DrawColorName = null;
+
+		DrawColor = new(255, 255, 255, 255);
+
+		CornerWidthPercent = 0;
+		CornerHeightPercent = 0;
+
+		TextureID = Surface.CreateNewTextureID();
 	}
 
-	void SetImage(ReadOnlySpan<char> imageName) { }
+	// FIXME #37
+	public override void Dispose() {
+		base.Dispose();
+
+		if (TextureID != TextureID.INVALID) {
+			// Surface.DeleteTextureID(TextureID);
+			TextureID = TextureID.INVALID;
+		}
+	}
+
+	void SetImage(ReadOnlySpan<char> imageName) {
+		if (!imageName.IsEmpty) {
+			Span<char> image = stackalloc char[MAX_PATH];
+			ReadOnlySpan<char> dir = "vgui/";
+			sprintf(image, "%s%s").S(dir).S(imageName);
+
+			if (ImageName != null && stricmp(ImageName, image) == 0)
+				return;
+
+			ImageName = image.ToString();
+		}
+		else
+			ImageName = null;
+
+		InvalidateLayout();
+	}
 
 	public override void PaintBackground() { }
 
