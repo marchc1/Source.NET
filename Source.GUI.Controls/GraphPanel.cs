6a75e060d83bcbb96c6cd5f81ec2718a153c368d
callumok2004 <callumokane123@gmail.com>
2026-01-01 22:48:47 +0000

    GraphPanel impl

--- Source.GUI.Controls/GraphPanel.cs
+++ Source.GUI.Controls/GraphPanel.cs
@@ -22,28 +22,151 @@ class GraphPanel : Panel
 	int GraphBarGapWidth;
 
 	public GraphPanel(Panel parent, ReadOnlySpan<char> name) : base(parent, name) {
-
+		DomainSize = 100.0f;
+		LowRange = 0.0f;
+		HighRange = 1.0f;
+		UseDynamicRange = true;
+		MinDomainSize = 0.0f;
+		MaxDomainSize = 0.0f;
+		MaxDomainSizeSet = false;
+		GraphBarWidth = 2;
+		GraphBarGapWidth = 2;
 	}
 
-	void SetDisplayDomainSize(float size) { }
+	void SetDisplayDomainSize(float size) {
+		DomainSize = size;
 
-	void SetMinDomainSize(float size) { }
+		if (!MaxDomainSizeSet)
+			SetMaxDomainSize(size);
+	}
 
-	void SetMaxDomainSize(float size) { }
+	void SetMinDomainSize(float size) => MinDomainSize = size;
 
-	void SetUseFixedRange(float lowRange, float highRange) { }
+	void SetMaxDomainSize(float size) {
+		MaxDomainSize = size;
+		MaxDomainSizeSet = true;
+	}
 
-	void SetUseDynamicRange(float rangeList, int numRanges) { }
+	void SetUseFixedRange(float lowRange, float highRange) {
+		UseDynamicRange = false;
+		LowRange = lowRange;
+		HighRange = highRange;
+	}
 
-	void GetDisplayedRange(float lowRange, float highRange) { }
+	void SetUseDynamicRange(float rangeList, int numRanges) {
+		UseDynamicRange = true;
+		RangeList.Clear();
+		for (int i = 0; i < numRanges; i++)
+			RangeList.Add(rangeList);
+	}
 
-	void AddItem(float sampleEnd, float sampleValue) { }
+	void GetDisplayedRange(out float lowRange, out float highRange) {
+		lowRange = LowRange;
+		highRange = HighRange;
+	}
 
-	// int GetVisibleItemCount() { }
+	public void AddItem(float sampleEnd, float sampleValue) {
+		if (Samples.Count > 0 && Samples.Last!.Value.Value == sampleValue) {
+			var last = Samples.Last;
+			last.Value = new Sample {
+				Value = last.Value.Value,
+				SampleEnd = sampleEnd
+			};
+		}
+		else
+			Samples.AddLast(new Sample {
+				Value = sampleValue,
+				SampleEnd = sampleEnd
+			});
 
-	public override void PerformLayout() { }
+		if (MaxDomainSizeSet) {
+			float freePoint = sampleEnd - MaxDomainSize;
+			while (Samples.Count > 0 && Samples.First!.Value.SampleEnd < freePoint)
+				Samples.RemoveFirst();
+		}
 
-	public override void Paint() { }
+		InvalidateLayout();
+		Repaint();
+	}
 
-	public override void ApplySchemeSettings(IScheme scheme) { }
+
+	int GetVisibleItemCount() => GetWide() / (GraphBarWidth + GraphBarGapWidth);
+
+	public override void Paint() {
+		if (Samples.Count == 0)
+			return;
+
+		var sampleNode = Samples.Last;
+		int x = GetWide() - (GraphBarWidth + GraphBarGapWidth);
+
+		float sampleSize = DomainSize / GetVisibleItemCount();
+
+		float resampleStart = sampleNode!.Value.SampleEnd - sampleSize;
+		resampleStart -= (float)(resampleStart % sampleSize);
+
+		float barSizeMultiplier = GetTall() / (HighRange - LowRange);
+
+		Surface.DrawSetColor(GetFgColor());
+
+		float minValue = Samples.First!.Value.Value;
+		float maxValue = Samples.First.Value.Value;
+
+		while (x > 0 && sampleNode != null) {
+			x -= GraphBarWidth + GraphBarGapWidth;
+
+			float value = 0f;
+			float maxSampleValue = 0f;
+			int samplesTouched = 0;
+
+			var prevNode = sampleNode.Previous;
+			while (prevNode != null) {
+				float v = sampleNode.Value.Value;
+
+				value += v;
+				samplesTouched++;
+
+				if (v < minValue)
+					minValue = v;
+				if (v > maxValue)
+					maxValue = v;
+				if (v > maxSampleValue)
+					maxSampleValue = v;
+
+				if (resampleStart < prevNode.Value.SampleEnd) {
+					sampleNode = prevNode;
+					prevNode = sampleNode.Previous;
+				}
+				else {
+					resampleStart -= sampleSize;
+					break;
+				}
+			}
+
+			int size = (int)(maxSampleValue * barSizeMultiplier);
+			Surface.DrawFilledRect(x, GetTall() - size, x + GraphBarWidth, GetTall());
+		}
+
+		if (UseDynamicRange) {
+			minValue = 0;
+
+			for (int i = 0; i < RangeList.Count; i++) {
+				if (RangeList[i] > maxValue) {
+					maxValue = RangeList[i];
+					break;
+				}
+			}
+
+			LowRange = minValue;
+			HighRange = maxValue;
+		}
+	}
+
+
+	public override void ApplySchemeSettings(IScheme scheme) {
+		base.ApplySchemeSettings(scheme);
+
+		SetFgColor(GetSchemeColor("GraphPanel.FgColor", scheme));
+		SetBgColor(GetSchemeColor("GraphPanel.BgColor", scheme));
+		SetBorder(scheme.GetBorder("ButtonDepressedBorder"));
+	}
 }
