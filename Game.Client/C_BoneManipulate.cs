0c5dababa205f158b7f8dff5ef7bfdf64eeee32c
march <106459595+marchc1@users.noreply.github.com>
2026-01-24 11:38:46 -0800

    Try fixing BoneManipulate

--- Game.Client/C_BoneManipulate.cs
+++ Game.Client/C_BoneManipulate.cs
@@ -3,19 +3,20 @@ using Source;
 using Game.Shared;
 using System.Numerics;
 namespace Game.Client;
+
 using FIELD = FIELD<C_BoneManipulate>;
 public class C_BoneManipulate : C_BaseEntity
 {
 	public static readonly RecvTable DT_BoneManipulate = new(DT_BaseEntity, [
-		RecvPropVector(FIELD.OF(nameof(BonePos))),
-		RecvPropVector(FIELD.OF(nameof(BoneAng))),
-		RecvPropVector(FIELD.OF(nameof(BoneScale))),
-		RecvPropInt(FIELD.OF(nameof(BoneJiggle))),
+		RecvPropArray3(FIELD.OF_ARRAY(nameof(BonePos)), RecvPropVector(null!)),
+		RecvPropArray3(FIELD.OF_ARRAY(nameof(BoneAng)), RecvPropVector(null!)),
+		RecvPropArray3(FIELD.OF_ARRAY(nameof(BoneScale)), RecvPropVector(null!)),
+		RecvPropArray3(FIELD.OF_ARRAY(nameof(BoneJiggle)), RecvPropInt(null!, null!)),
 	]);
 	public static readonly new ClientClass ClientClass = new ClientClass("BoneManipulate", DT_BoneManipulate).WithManualClassID(StaticClassIndices.CBoneManipulate);
 
-	public Vector3 BonePos;
-	public Vector3 BoneAng;
-	public Vector3 BoneScale;
-	public int BoneJiggle;
+	public InlineArrayMaxStudioBones<Vector3> BonePos;
+	public InlineArrayMaxStudioBones<Vector3> BoneAng;
+	public InlineArrayMaxStudioBones<Vector3> BoneScale;
+	public InlineArrayMaxStudioBones<int> BoneJiggle;
 }
