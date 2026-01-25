16b8c94cbc8222ef89c9040683e03d1de1730b14
march <106459595+marchc1@users.noreply.github.com>
2026-01-24 19:54:24 -0800

    AirMove/AirAccelerate

--- Game.Shared/GameMovement.cs
+++ Game.Shared/GameMovement.cs
@@ -257,9 +257,92 @@ public class GameMovement : IGameMovement
 	// Handles both ground friction and water friction
 	protected void Friction() { throw new NotImplementedException(); }
 
-	protected virtual void AirAccelerate(ref Vector3 wishdir, float wishspeed, float accel) { throw new NotImplementedException(); }
+	protected virtual void AirAccelerate(ref Vector3 wishdir, float wishspeed, float accel) {
+		int i;
+		float addspeed, accelspeed, currentspeed;
+		float wishspd;
 
-	protected virtual void AirMove() { throw new NotImplementedException(); }
+		wishspd = wishspeed;
+
+		if (Player.pl.DeadFlag)
+			return;
+
+		if (Player.WaterJumpTime != 0)
+			return;
+
+		// Cap speed
+		if (wishspd > GetAirSpeedCap())
+			wishspd = GetAirSpeedCap();
+
+		// Determine veer amount
+		currentspeed = mv!.Velocity.Dot(wishdir);
+
+		// See how much to add
+		addspeed = wishspd - currentspeed;
+
+		// If not adding any, done.
+		if (addspeed <= 0)
+			return;
+
+		// Determine acceleration speed after acceleration
+		accelspeed = accel * wishspeed * (float)gpGlobals.FrameTime* Player.SurfaceFriction;
+
+		// Cap it
+		if (accelspeed > addspeed)
+			accelspeed = addspeed;
+
+		// Adjust pmove vel.
+		for (i = 0; i < 3; i++) {
+			mv.Velocity[i] += accelspeed * wishdir[i];
+			mv.WishVel[i] += accelspeed * wishdir[i];
+		}
+	}
+
+	protected virtual void AirMove() {
+		int i;
+		Vector3 wishvel = default;
+		float fmove, smove;
+		Vector3 wishdir;
+		float wishspeed;
+		Vector3 forward, right, up;
+
+		MathLib.AngleVectors(mv!.ViewAngles, out forward, out right, out up);  // Determine movement angles
+
+		// Copy movement amounts
+		fmove = mv.ForwardMove;
+		smove = mv.SideMove;
+
+		// Zero out z components of movement vectors
+		forward[2] = 0;
+		right[2] = 0;
+		MathLib.VectorNormalize(ref forward);  // Normalize remainder of vectors
+		MathLib.VectorNormalize(ref right);    // 
+
+		for (i = 0; i < 2; i++)       // Determine x and y parts of velocity
+			wishvel[i] = forward[i] * fmove + right[i] * smove;
+		wishvel[2] = 0;             // Zero out z part of velocity
+
+		MathLib.VectorCopy(wishvel, out wishdir);   // Determine maginitude of speed of move
+		wishspeed = MathLib.VectorNormalize(ref wishdir);
+
+		//
+		// clamp to server defined max speed
+		//
+		if (wishspeed != 0 && (wishspeed > mv.MaxSpeed)) {
+			MathLib.VectorScale(wishvel, mv.MaxSpeed / wishspeed, out wishvel);
+			wishspeed = mv.MaxSpeed;
+		}
+
+		AirAccelerate(ref wishdir, wishspeed, sv_airaccelerate.GetFloat());
+
+		// Add in any base velocity to the current velocity.
+		MathLib.VectorAdd(mv.Velocity, Player.GetBaseVelocity(), out mv.Velocity);
+
+		TryPlayerMove();
+
+		// Now pull the base velocity back out.   Base velocity is set if you are on a moving object, like a conveyor (or maybe another monster?)
+		MathLib.VectorSubtract(mv.Velocity, Player.GetBaseVelocity(), out mv.Velocity);
+	}
 	protected virtual float GetAirSpeedCap() { return 30f; }
 
 	protected virtual bool CanAccelerate() {
