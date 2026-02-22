a220d2b6fdbdc184f258c71e4a93729ce69c67ba
callumok2004 <callumokane123@gmail.com>
2026-02-22 01:07:30 +0000

    Impl PEntityOfEntIndex

--- Source.Engine/EngineServerImpl.cs
+++ Source.Engine/EngineServerImpl.cs
@@ -388,8 +388,14 @@ internal class EngineServer(Cbuf Cbuf) : IEngineServer
 		throw new NotImplementedException();
 	}
 
-	public Edict PEntityOfEntIndex(int iEntIndex) {
-		throw new NotImplementedException();
+	public Edict? PEntityOfEntIndex(int iEntIndex) {
+		if (iEntIndex >= 0 && iEntIndex < sv.MaxEdicts) {
+			Edict? edict = sv.Edicts![iEntIndex];
+			if (edict.IsFree())
+				return edict;
+		}
+
+		return null;
 	}
 
 	public void PlaybackTempEntity(IRecipientFilter filter, float delay, object sender, SendTable st, int classID) {
