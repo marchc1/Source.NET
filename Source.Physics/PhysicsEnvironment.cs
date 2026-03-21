2e7666c8986262efb2ddbfe9f59bde292195ce1b
march <106459595+marchc1@users.noreply.github.com>
2026-03-20 20:31:53 -0700

    Create the environment

--- Source.Physics/PhysicsEnvironment.cs
+++ Source.Physics/PhysicsEnvironment.cs
@@ -1,8 +1,19 @@
 ﻿global using static Source.Physics.PhysicsEnvironmentGlobals;
 
+using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuPhysics.CollisionDetection;
+using BepuPhysics.Constraints;
+
+using BepuUtilities;
+using BepuUtilities.Memory;
+
 using Source.Common;
 using Source.Common.Mathematics;
 using Source.Common.Physics;
+
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Source.Physics;
 
@@ -13,8 +24,82 @@ internal static class PhysicsEnvironmentGlobals
 	internal static IPhysicsObjectPairHash CreateObjectPairHash() => new ObjectPairHash();
 }
 
+internal struct PhysicsNarrowPhaseCallbacks(PhysicsEnvironment env) : INarrowPhaseCallbacks
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool AllowContactGeneration(int workerIndex, CollidableReference a, CollidableReference b, ref float speculativeMargin) {
+		return a.Mobility == CollidableMobility.Dynamic || b.Mobility == CollidableMobility.Dynamic;
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool AllowContactGeneration(int workerIndex, CollidablePair pair, int childIndexA, int childIndexB) {
+		return true;
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool ConfigureContactManifold<TManifold>(int workerIndex, CollidablePair pair, ref TManifold manifold, out PairMaterialProperties pairMaterial) where TManifold : unmanaged, IContactManifold<TManifold> {
+		pairMaterial.FrictionCoefficient = 1f;
+		pairMaterial.MaximumRecoveryVelocity = 2f;
+		pairMaterial.SpringSettings = new(30, 1);
+		return true;
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool ConfigureContactManifold(int workerIndex, CollidablePair pair, int childIndexA, int childIndexB, ref ConvexContactManifold manifold) {
+		return true;
+	}
+
+	public void Dispose() {
+
+	}
+
+	public void Initialize(Simulation simulation) {
+
+	}
+}
+
+internal struct PhysicsPoseIntegratorCallbacks(PhysicsEnvironment env) : IPoseIntegratorCallbacks
+{
+	public AngularIntegrationMode AngularIntegrationMode => AngularIntegrationMode.Nonconserving;
+	public bool AllowSubstepsForUnconstrainedBodies => true;
+	public bool IntegrateVelocityForKinematics => false;
+	public void Initialize(Simulation simulation) {
+
+	}
+
+	// TODO: All of this is copy pasted from a bepuphysics example. Make this work closer to Source...
+	public float LinearDamping = 0.03f;
+	public float AngularDamping = 0.03f;
+
+	Vector3Wide gravityWideDt;
+	Vector<float> linearDampingDt;
+	Vector<float> angularDampingDt;
+
+	public void IntegrateVelocity(Vector<int> bodyIndices, Vector3Wide position, QuaternionWide orientation, BodyInertiaWide localInertia, Vector<int> integrationMask, int workerIndex, Vector<float> dt, ref BodyVelocityWide velocity) {
+		velocity.Linear = (velocity.Linear + gravityWideDt) * linearDampingDt;
+		velocity.Angular = velocity.Angular * angularDampingDt;
+	}
+
+	public void PrepareForIntegration(float dt) {
+		linearDampingDt = new Vector<float>(MathF.Pow(MathHelper.Clamp(1 - LinearDamping, 0, 1), dt));
+		angularDampingDt = new Vector<float>(MathF.Pow(MathHelper.Clamp(1 - AngularDamping, 0, 1), dt));
+		env.GetGravity(out var grav);
+		gravityWideDt = Vector3Wide.Broadcast(grav * dt);
+	}
+}
+
 internal class PhysicsEnvironment : IPhysicsEnvironment
 {
+	readonly PhysicsNarrowPhaseCallbacks narrowPhaseCallbacks;
+	readonly PhysicsPoseIntegratorCallbacks poseIntegratorCallbacks;
+	readonly Simulation Simulation;
+	readonly BufferPool BufferPool;
+	readonly SolveDescription SolveDescription;
+
+	public PhysicsEnvironment() {
+		BufferPool = new();
+		narrowPhaseCallbacks = new(this);
+		poseIntegratorCallbacks = new(this);
+		SolveDescription = new SolveDescription();
+		Simulation = Simulation.Create(BufferPool, narrowPhaseCallbacks, poseIntegratorCallbacks, SolveDescription);
+	}
 
 	public void CleanupDeleteList() {
 		throw new NotImplementedException();
@@ -241,7 +326,7 @@ internal class PhysicsEnvironment : IPhysicsEnvironment
 	}
 
 	public void SetGravity(in AngularImpulse gravityVector) {
-
+		throw new NotImplementedException();
 	}
 
 	public void SetObjectEventHandler(IPhysicsObjectEvent objectEvents) {
@@ -257,7 +342,7 @@ internal class PhysicsEnvironment : IPhysicsEnvironment
 	}
 
 	public void SetSimulationTimestep(double timestep) {
-
+		throw new NotImplementedException();
 	}
 
 	public void Simulate(double deltaTime) {
