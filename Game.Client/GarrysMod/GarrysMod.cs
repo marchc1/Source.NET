a273f2b5cd58a75be69a24311bd90f9f209a56a1
callumok2004 <callumokane123@gmail.com>
2026-07-23 06:02:36 +0100

    Steam rich presence

--- Game.Client/GarrysMod/GarrysMod.cs
+++ Game.Client/GarrysMod/GarrysMod.cs
@@ -1,8 +1,14 @@
 ﻿global using static Game.Client.GarrysMod.GarrysModSingletons;
 
+using Game.Shared;
+
 using Microsoft.Extensions.DependencyInjection;
 
+using Source;
 using Source.Common.MaterialSystem;
+using Source.Common.Networking;
+
+using Steamworks;
 
 using System;
 using System.Collections.Generic;
@@ -10,7 +16,8 @@ using System.Text;
 
 namespace Game.Client.GarrysMod;
 
-public static class GarrysModSingletons{
+public static class GarrysModSingletons
+{
 	public static readonly GarrysMod garrysmod = new();
 }
 
@@ -20,7 +27,79 @@ public class GarrysMod
 
 	}
 
-	public void InitializeMod(IServiceProvider services){
+	public void InitializeMod(IServiceProvider services) {
 
+	}
+}
+
+public class GModRichPresence : AutoGameSystemPerFrame
+{
+	private static readonly GModRichPresence _ = new();
+
+	string LastStatus = "";
+	TimeUnit_t LastRun;
+
+	public override ReadOnlySpan<char> Name() => "GModRichPresence";
+
+	public override bool Init() {
+		LastStatus = "";
+		return true;
+	}
+
+	public override void Shutdown() {
+		if (SteamAPI.IsSteamRunning())
+			SteamFriends.ClearRichPresence();
+	}
+
+	public override void Update(TimeUnit_t frametime) {
+		if (gpGlobals.RealTime < LastRun + 2.0f) return;
+		LastRun = gpGlobals.RealTime;
+
+		if (!SteamAPI.IsSteamRunning())
+			return;
+
+		if (!engine.IsConnected()) {
+			SetStatus("In Menus");
+			return;
+		}
+
+		if (!engine.IsInGame()) {
+			SetStatus("Joining a server");
+			return;
+		}
+
+		bool multiplayer = engine.GetMaxClients() > 1;
+		string? connect = multiplayer && engine.GetNetChannelInfo() is INetChannel netchan && netchan.GetRemoteAddress() is NetAddress address ? $"+connect {address.ToString(false)}" : null;
+
+		SetStatus($"{(multiplayer ? "Multiplayer" : "Singleplayer")} - {GetMapName()} ({GetGamemodeName()})", connect);
+	}
+
+	void SetStatus(string status, string? connect = null) {
+		if (status == LastStatus)
+			return;
+		LastStatus = status;
+
+		SteamFriends.SetRichPresence("status", status);
+		SteamFriends.SetRichPresence("Generic", status);
+		SteamFriends.SetRichPresence("steam_display", "#Status_Generic");
+		SteamFriends.SetRichPresence("connect", connect);
+	}
+
+	static ReadOnlySpan<char> GetGamemodeName() {
+		return "GAMEMODE"; // TODO
+	}
+
+	static ReadOnlySpan<char> GetMapName() {
+		ReadOnlySpan<char> level = engine.GetLevelName().SliceNullTerminatedString();
+		if (level.IsEmpty)
+			return level;
+
+		int slash = level.LastIndexOfAny('/', '\\');
+		if (slash >= 0) level = level[(slash + 1)..];
+
+		int dot = level.LastIndexOf('.');
+		if (dot >= 0) level = level[..dot];
+
+		return level;
 	}
 }
