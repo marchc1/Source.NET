b66234a955ced06f3b14dd8c9a2281fb1a26256a
march <106459595+marchc1@users.noreply.github.com>
2026-07-24 12:52:43 -0700

    GMOD.IsValidPath

---
+++ Source.Common/GarrysMod/GMOD.cs
@@ -0,0 +1,41 @@
+﻿using Source.Common.Hashing;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Source.Common.GarrysMod;
+
+public static class GMOD
+{
+	public static bool IsValidPath(ReadOnlySpan<char> inputPath) {
+		if (inputPath.IsEmpty)
+			return false;
+
+		char prev = '\0';
+		for (int i = 0; i < inputPath.Length; i++) {
+			char c = inputPath[i];
+			switch (c) {
+				case '\0':
+				case '\n':
+				case '\r':
+				case '\t':
+				case ':':
+				case '?':
+				case '|':
+				case '>':
+				case '<':
+				case '"':
+					return false;
+				case '\\':
+					if (prev == '\\') return false;
+					break;
+				case '.':
+					if (prev == '.') return false;
+					break;
+			}
+			prev = c;
+		}
+		return true;
+	}
+}
